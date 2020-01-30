f20205a79ef8232904a83627ad4080e82830fdd3
fmorgante <fmorgante@uchicago.edu>
2020-01-30 12:58:50 -0600

    Start adding unit tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(mr.mash.alpha)
+
+test_check("mr.mash.alpha")
---
+++ tests/testthat/test_mash_regression.R
@@ -0,0 +1,50 @@
+context("Test MASH regression wrapper")
+
+test_that("bayes_mvr_mix and bayes_mvr_mash return the same results", {
+  ###Set seed
+  set.seed(123)
+
+  ###Simulate X and Y
+  n  <- 100
+  p <- 10
+
+  ###Set residual covariance
+  V  <- rbind(c(1.0,0.2),
+              c(0.2,0.4))
+
+  ###Set true effects
+  B  <- matrix(c(-2, -2,
+                  5, 5,
+                  rep(0, (p-2)*2)), byrow=T, ncol=2)
+  
+  ###Simulate X
+  X <- matrix(rnorm(n*p), nrow=n, ncol=p)
+  X <- scale(X, center=T, scale=F)
+
+  ###Simulate Y from MN(XB, I_n, V) where I_n is an nxn identity matrix and V is the residual covariance
+  Y <- sim_mvr(X, B, V)
+
+  ###Specify the mixture weights and covariance matrices for the mixture-of-normals prior
+  grid <- seq(1, 5)
+  S0mix <- compute_cov_canonical(ncol(Y), singletons=T, hetgrid=c(0, 0.25, 0.5, 0.75, 0.99), grid)
+  names(S0mix) <- paste0("S0_", seq(1, length(S0mix)))
+  zero_mat <- matrix(0, ncol(Y), ncol(Y))
+  zero_mat[upper.tri(zero_mat)] <- 1e-10
+  zero_mat[lower.tri(zero_mat)] <- 1e-10
+  S0mix[[paste0("S0_", length(S0mix)+1)]] <- zero_mat
+
+  w0    <- rep(1/(length(S0mix)), length(S0mix))
+
+  ###Fit my function
+  fit_mix <- bayes_mvr_mix(X[, 1], Y, V, w0, S0mix)
+
+  ###Fit MASH wrapper
+  fit_mash <- bayes_mvr_mash(X[, 1], Y, V, w0, S0mix)
+
+  ###Tests
+  expect_equal(fit_mix$mu1, fit_mash$mu1, tolerance = 1e-10, scale = 1)
+  expect_equal(fit_mix$S1, fit_mash$S1, tolerance = 1e-10, scale = 1)
+  expect_equal(fit_mix$w1, fit_mash$w1, tolerance = 1e-10, scale = 1)
+  expect_equal(fit_mix$logbf, fit_mash$logbf, tolerance = 1e-10, scale = 1)
+})
+
