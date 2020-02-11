799cad365adab52152c7633f32628658a849c330
Fabio Morgante <42005156+fmorgante@users.noreply.github.com>
2020-02-11 11:32:52 -0600

    Add functions for scaled X

--- R/bayes_reg_mv.R
+++ R/bayes_reg_mv.R
@@ -40,6 +40,32 @@ bayes_mvr_ridge <- function (x, Y, V, S0) {
 }
 
 
+# Bayesian multivariate regression with Normal prior with scaled X
+#
+# The outputs are: b, the least-squares estimate of the regression
+# coefficients; S, the covariance of b; mu1, the posterior mean of the
+# regression coefficients; S1, the posterior covariance of the
+# regression coefficients; logbf, the log-Bayes factor.
+bayes_mvr_ridge_scaled_X <- function (x, Y, S0, S, S1, SplusS0_chol, S_chol) {
+  
+  # Compute the least-squares estimate.
+  n <- nrow(Y)
+  b <- drop(x %*% Y)/(n-1)
+  
+  # Compute the log-Bayes factor.
+  logbf <- (chol2ldet(S_chol) - chol2ldet(SplusS0_chol) +
+              dot(b,backsolve(S_chol, forwardsolve(t(S_chol), b))) - 
+              dot(b,backsolve(SplusS0_chol, forwardsolve(t(SplusS0_chol), b))))/2
+  
+  # Compute the posterior mean assuming a multivariate
+  # normal prior with zero mean and covariance S0.
+  mu1 <- drop(S1%*%backsolve(S_chol, forwardsolve(t(S_chol), b)))
+  
+  # Return the least-squares estimate (b), the posterior mean
+  # (mu1), and the log-Bayes factor (logbf)
+  return(list(b = b, mu1 = mu1, logbf = logbf))
+}
+
 
 # Bayesian multivariate regression with spike-and-slab prior 
 #
@@ -128,6 +154,67 @@ bayes_mvr_mix <- function (x, Y, V, w0, S0) {
   return(list(logbf = logbf_mix,w1 = w1,mu1 = mu1_mix,S1 = S1_mix))
 }
 
+                  
+                  
+# Bayesian multivariate regression with mixture-of-normals prior
+# (mixture weights w0 and covariance matrices S0) with scaled X.
+#
+# The outputs are: the log-Bayes factor (logbf), the posterior assignment probabilities
+# (w1), the posterior mean of the coefficients given that all the
+# coefficients are not nonzero (mu1), and the posterior covariance of
+# the coefficients given that all the coefficients are not zero (S1).
+bayes_mvr_mix_scaled_X <- function (x, Y, w0, S0, S, S1, SplusS0_chol, S_chol) {
+  
+  # Get the number of variables (n) and the number of mixture
+  # components (k).
+  R <- ncol(Y)
+  K <- length(S0)
+  
+  # Compute the Bayes factors and posterior statistics separately for
+  # each mixture component.
+  # out <- vector("list",K)
+  # for (k in 1:K){
+  #   out[[k]] <- bayes_mvr_ridge(x,Y,V,S0[[k]])
+  # }
+  bayes_mvr_ridge_lapply <- function(i){
+    bayes_mvr_ridge(x, Y, S0[[i]], S, S1[[i]], SplusS0_chol[[i]], S_chol)
+  }
+  out <- lapply(1:K, bayes_mvr_ridge_lapply)
+  
+  # Compute the posterior assignment probabilities for the latent
+  # indicator variable.
+  logbf <- sapply(out,function (x) x$logbf)
+  w1    <- softmax(logbf + log(w0))
+  
+  # Compute the posterior mean (mu1_mix) and covariance (S1_mix) of the
+  # regression coefficients.
+  A   <- matrix(0,R,R)
+  mu1_mix <- rep(0,R)
+  for (k in 1:K) {
+    wk  <- w1[k]
+    muk <- out[[k]]$mu1
+    Sk  <- S1[[k]]
+    mu1_mix <- mu1_mix + wk*muk
+    A   <- A   + wk*(Sk + tcrossprod(muk))
+  }
+  S1_mix <- A - tcrossprod(mu1_mix)
+  ##The following code does not work in the univariate case
+  # muk <- t(sapply(out, function (x) x$mu1))
+  # Sk <- lapply(out, function (x) x$S1)
+  # mu1_mix <- colSums(muk*w1)
+  # S1_mix <- Reduce("+", lapply(1:K, function(i){w1[i]*(Sk[[i]] + tcrossprod(muk[i, ]))})) - tcrossprod(mu1_mix)
+  
+  # Compute the log-Bayes factor for the mixture as a linear combination of the
+  # individual BFs foreach mixture component.
+  u     <- max(logbf)
+  logbf_mix <- u + log(sum(w0 * exp(logbf - u)))
+  
+  # Return the log-Bayes factor for the mixture (logbf), the posterior assignment probabilities (w1), the
+  # posterior mean of the coefficients (mu1), and the posterior
+  # covariance of the coefficients (S1).
+  return(list(logbf = logbf_mix,w1 = w1,mu1 = mu1_mix,S1 = S1_mix))
+}
+                  
 # Bayesian multivariate regression with mixture-of-normals prior
 # (mixture weights w0 and covariance matrices S0) using MASH
 # TO DO: Move MashInitializer$new outside the function when using in mr.mash
