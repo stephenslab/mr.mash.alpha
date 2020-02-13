3250243789291baf43664819d1f2ee2675ec3b6d
fmorgante <fmorgante@uchicago.edu>
2020-02-13 10:32:35 -0600

    Fix typo

--- R/bayes_reg_mv.R
+++ R/bayes_reg_mv.R
@@ -177,7 +177,7 @@ bayes_mvr_mix_scaled_X <- function (x, Y, w0, S0, S, S1, SplusS0_chol, S_chol, l
   #   out[[k]] <- bayes_mvr_ridge(x,Y,V,S0[[k]])
   # }
   bayes_mvr_ridge_lapply <- function(i){
-    bayes_mvr_ridge_scaled_X(x, Y, S0[[i]], S, S1[[i]], SplusS0_chol[[i]], S_chol, ldetSplusS0_chol, ldetS_chol)
+    bayes_mvr_ridge_scaled_X(x, Y, S0[[i]], S, S1[[i]], SplusS0_chol[[i]], S_chol, ldetSplusS0_chol[i], ldetS_chol)
   }
   out <- lapply(1:K, bayes_mvr_ridge_lapply)
   
