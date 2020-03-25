59e4b103063a2d5ee44dae9c712a96c7fd34ff57
fmorgante <fmorgante@uchicago.edu>
2020-03-25 14:33:21 -0500

    Use mvtnorm::rmvnorm

--- R/simulate_demo_data.R
+++ R/simulate_demo_data.R
@@ -11,6 +11,9 @@
 ## Gamma_scale=scaling factor for Sigma;
 ## V_offdiag=value of the off-diagonal elements of V;
 ## V_scale=scaling factor for Sigma.
+##
+#' @importFrom mvtnorm rmvnorm
+#' 
 simulate_mr_mash_data <- function(n, p, p_causal, r, 
                                   Sigma_offdiag=1, Sigma_scale=0.8,
                                   Gamma_offdiag=0, Gamma_scale=0.8,
@@ -21,13 +24,13 @@ simulate_mr_mash_data <- function(n, p, p_causal, r,
   
   ##Simulate true effects from N_r(0, Sigma) where Sigma is a given covariance matrix across traits
   Sigma <- create_cov_canonical(r, singletons=FALSE, hetgrid=Sigma_offdiag)
-  B_causal <- MASS::mvrnorm(n=p_causal, mu=rep(0, r), Sigma=Sigma_scale*Sigma[[1]])
+  B_causal <- rmvnorm(n=p_causal, mean=rep(0, r), sigma=Sigma_scale*Sigma[[1]])
   B <- matrix(0, ncol=r, nrow=p)
   B[1:p_causal, ] <- B_causal
   
   ##Simulate X from N_r(0, Gamma) where Gamma is a given covariance matrix across variables
   Gamma <- create_cov_canonical(p, singletons=FALSE, hetgrid=Gamma_offdiag)
-  X <- MASS::mvrnorm(n=n, mu=rep(0, p), Sigma=Gamma_scale*Gamma[[1]])
+  X <- rmvnorm(n=n, mean=rep(0, p), sigma=Gamma_scale*Gamma[[1]])
   X <- scale_fast2(X, scale=FALSE)$M
   
   ##Simulate Y from MN(XB, I_n, V) where I_n is an nxn identity matrix and V is the residual covariance  
