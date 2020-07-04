51776c23ff4b662239d032284e5f80e5cb96b673
fmorgante <fmorgante@uchicago.edu>
2020-07-03 19:22:03 -0500

    Fix test

--- tests/testthat/test_compute_rank_variables_BFmix_R_vs_Rcpp.R
+++ tests/testthat/test_compute_rank_variables_BFmix_R_vs_Rcpp.R
@@ -39,19 +39,19 @@ test_that("R and Rcpp version of compute_rank_variables_BFmix return the same re
   
   ###Compute logbf with standardize=TRUE
   comps_r <- precompute_quants(X, V, S0mix, standardize=TRUE, version="R")
-  ranks_r <- compute_rank_variables_BFmix(X, Y, V, Vinv, w0, S0mix, comps_r, standardize=TRUE, version="R", decreasing=TRUE, eps)
+  ranks_r <- compute_rank_variables_BFmix(X, Y, V, Vinv, w0, S0mix, comps_r, standardize=TRUE, version="R", decreasing=TRUE, eps, nthreads=1)
   
   comps_rcpp <- precompute_quants(X, V, S0mix, standardize=TRUE, version="Rcpp")
-  ranks_rcpp <- compute_rank_variables_BFmix(X, Y, V, Vinv, w0, S0mix, comps_rcpp, standardize=TRUE, version="Rcpp", decreasing=TRUE, eps)
+  ranks_rcpp <- compute_rank_variables_BFmix(X, Y, V, Vinv, w0, S0mix, comps_rcpp, standardize=TRUE, version="Rcpp", decreasing=TRUE, eps, nthreads=1)
   
   ###Compute logbf with standardize=FALSE
   comps1_r <- precompute_quants(X, V, S0mix, standardize=FALSE, version="R")
   comps1_r$xtx <- colSums(X^2)
-  ranks1_r <- compute_rank_variables_BFmix(X, Y, V, Vinv, w0, S0mix, comps1_r, standardize=FALSE, version="R", decreasing=TRUE, eps)
+  ranks1_r <- compute_rank_variables_BFmix(X, Y, V, Vinv, w0, S0mix, comps1_r, standardize=FALSE, version="R", decreasing=TRUE, eps, nthreads=1)
   
   comps1_rcpp <- precompute_quants(X, V, S0mix, standardize=FALSE, version="Rcpp")
   comps1_rcpp$xtx <- colSums(X^2)
-  ranks1_rcpp <- compute_rank_variables_BFmix(X, Y, V, Vinv, w0, S0mix, comps1_rcpp, standardize=FALSE, version="Rcpp", decreasing=TRUE, eps)
+  ranks1_rcpp <- compute_rank_variables_BFmix(X, Y, V, Vinv, w0, S0mix, comps1_rcpp, standardize=FALSE, version="Rcpp", decreasing=TRUE, eps, nthreads=1)
   
   ###Tests
   expect_equal(ranks_r, ranks_rcpp, tolerance = 1e-10, scale = 1)
