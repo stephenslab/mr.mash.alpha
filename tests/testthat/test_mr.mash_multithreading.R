b32a36bfadd55771c9622fe72c74db84c04620b4
fmorgante <fmorgante@uchicago.edu>
2020-07-06 14:43:57 -0500

    Add test for multithreading

---
+++ tests/testthat/test_mr.mash_multithreading.R
@@ -0,0 +1,57 @@
+context("mr.mash with 1 or 2 thread(s) return the same results")
+
+test_that("mr.mash with 1 or 2 thread(s) return the same results", {
+  ###Set seed
+  set.seed(123)
+  
+  ###Simulate X and Y
+  n  <- 100
+  p <- 10
+  
+  ###Set residual covariance
+  V  <- rbind(c(1.0,0.2),
+              c(0.2,0.4))
+  
+  ###Set true effects
+  B  <- matrix(c(-2, -2,
+                 5, 5,
+                 rep(0, (p-2)*2)), byrow=TRUE, ncol=2)
+  
+  ###Simulate X
+  X <- matrix(rnorm(n*p), nrow=n, ncol=p)
+  X <- scale(X, center=TRUE, scale=FALSE)
+  
+  ###Simulate Y from MN(XB, I_n, V) where I_n is an nxn identity
+  ###matrix and V is the residual covariance
+  Y <- sim_mvr(X, B, V)
+  
+  ###Specify the mixture weights and covariance matrices for the
+  ###mixture-of-normals prior
+  grid <- seq(1, 5)
+  S0mix <- compute_cov_canonical(ncol(Y), singletons=TRUE,
+                                 hetgrid=c(0, 0.25, 0.5, 0.75, 0.99), grid,
+                                 zeromat=TRUE)
+  
+  w0    <- rep(1/(length(S0mix)), length(S0mix))
+  
+  ###Estimate residual covariance
+  V_est <- cov(Y)
+  
+  ###Fit with current implementation (1 thread)
+  capture.output(
+    fit_1 <- mr.mash(X, Y, S0mix, w0, V_est, update_w0=TRUE,
+                   update_w0_method="EM", compute_ELBO=TRUE, standardize=TRUE,
+                   verbose=FALSE, update_V=TRUE, nthreads=1))
+  fit_1$progress <- fit_1$progress[, -2] ##This line is needed to remove the timing column -->  
+                                         ##hopefully faster when using multiple threads
+  
+  ###Fit with current implementation (2 threads)
+  capture.output(
+    fit_2 <- mr.mash(X, Y, S0mix, w0, V_est, update_w0=TRUE,
+                     update_w0_method="EM", compute_ELBO=TRUE, standardize=TRUE,
+                     verbose=FALSE, update_V=TRUE, nthreads=2))
+  fit_2$progress <- fit_2$progress[, -2]
+  
+  ###Test
+  expect_equal(fit_1, fit_2, tolerance=1e-10, scale=1)
+})
